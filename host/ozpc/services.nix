e3d1abc4f6030183feeebdc65264207b6e17f7bf
Oz Browning <56755170+ozmodeuz@users.noreply.github.com>
2024-10-25 10:22:09 +0100

    add minecraft routes to cloudflared

--- host/ozpc/services.nix
+++ host/ozpc/services.nix
@@ -32,6 +32,10 @@ in
         "ea690d15-f297-43bf-8f7d-0f6d11ee1f42" = {
           credentialsFile = config.sops.templates."cloudflared.json".path;
           default = "http_status:404";
+          ingress = {
+            "mc4eva.mrld.city" = "tcp://localhost:25565";
+            "skyblock.mrld.city" = "tcp://localhost:25566";
+          };
         };
       };
     };
