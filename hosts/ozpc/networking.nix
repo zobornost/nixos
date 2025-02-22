4c10da3c224e8f5ac3998bbdafac22ac3e608a8a
Oz Browning <56755170+ozmodeuz@users.noreply.github.com>
2025-02-22 14:41:42 +0000

    fw rules for deskflow

--- hosts/ozpc/networking.nix
+++ hosts/ozpc/networking.nix
@@ -2,5 +2,14 @@
 {
   networking = {
     hostName = "ozpc";
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        22
+        80
+        443
+        24800
+      ];
+    };
   };
 }
