9a860486c5c7b1ac3c954cf80d100e82122942e3
Oz Browning <56755170+ozmodeuz@users.noreply.github.com>
2025-02-22 14:41:25 +0000

    fix wave mic

--- hosts/ozpc/boot.nix
+++ hosts/ozpc/boot.nix
@@ -2,9 +2,27 @@
 {
   boot = {
     initrd = {
-      availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "uas" "sd_mod" ];
+      availableKernelModules = [
+        "vmd"
+        "xhci_pci"
+        "ahci"
+        "nvme"
+        "usbhid"
+        "usb_storage"
+        "uas"
+        "sd_mod"
+      ];
     };
-    kernelModules = [ "kvm-intel" "coretemp" "nct6775" ];
-    kernelParams = [ "quiet" "splash" "boot.shell_on_fail" ];
+    kernelModules = [
+      "kvm-intel"
+      "coretemp"
+      "nct6775"
+    ];
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "usbcore.autosuspend=-1"
+    ];
   };
 }
--- hosts/ozpc/services.nix
+++ hosts/ozpc/services.nix
@@ -1,6 +1,14 @@
 { ... }:
 {
   services = {
+    pipewire = {
+      enable = true;
+      alsa.enable = false;
+      alsa.support32Bit = false;
+      pulse.enable = true;
+      jack.enable = false;
+      wireplumber.enable = true;
+    };
     xserver = {
       videoDrivers = [ "nvidia" ];
     };
